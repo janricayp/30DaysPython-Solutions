c3febd33bfab5c15e27eeb7d4b0349a8b196d389
janricayp <janricayp@gmail.com>
2025-08-13 13:53:28 +0800

    Day 14 - High Order Functions

--- day_14/day-14-high-order-functions.py
+++ day_14/day-14-high-order-functions.py
@@ -53,3 +53,47 @@ for name in names:
 for num in numbers:
     print(num)
 
+# Exercises: Level 2
+# 1
+def uppercase(para):
+    return para.upper()
+
+countries_upper = list(map(lambda country: uppercase(country), countries))
+print(countries_upper)
+
+# 2
+square = list(map(lambda x: x ** 2, numbers))
+print(square)
+
+# 3
+names_upper = list(map(lambda name: name.upper(), names))
+print(names_upper)
+
+# 4
+land_countries = list(filter(lambda country: 'land' in country, countries))
+print(land_countries)
+
+# 5
+country_6_letters = list(filter(lambda country: len(country) == 6, countries))
+print(country_6_letters)
+
+# 6
+country_more_letters = list(filter(lambda country: len(country) >= 6, countries))
+print(country_more_letters)
+
+# 7
+e_countries = list(filter(lambda country: country[0] == 'E', countries))
+print(e_countries)
+
+# 8
+squared = map(lambda x: x ** 2, numbers)
+even_squares = filter(lambda x: x % 2 == 0, squared)
+result = reduce(lambda x, y: x + y, even_squares, 0)
+print(result)
+
+# 9
+def get_string_lists(lst):
+    strings = [str for str in lst if type(str) == "str"]
+    return strings
+strs = list(filter(get_string_lists, names))
+print(strs)
